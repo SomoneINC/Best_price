e5a6d0fc9493ed832b6e13f23e98052aabf4329d
SomoneINC <72033209+SomoneINC@users.noreply.github.com>
2024-05-02 13:38:00 +0200

    Update Alch.py

--- Alch.py
+++ Alch.py
@@ -1,7 +1,46 @@
 import pandas as pd
 import random
+import requests
+from bs4 import BeautifulSoup
 import os
-link = "https://www.rimi.lv/e-veikals/lv/produkti/alkoholiskie-dzerieni/c/SH-1?currentPage=1&pageSize=20&query=%3Arelevance%3AallCategories%3ASH-1%3AassortmentStatus%3AinAssortment%3AcategoryNamePath%3AKoktei%25C4%25BCi"
+
+class PC():
+    SpriritListSections = []
+    SpiritDrinkList = []
+
+link = "https://www.spiritsandwine.lv/lv/"
+req = requests.get(link)
+x = 0
+soup = BeautifulSoup(req.content, 'html.parser')
+selection = soup.find_all('div', class_="col-sm-5 col-md-4 col-xl-3 mb-1")
+for items in selection:
+    find_a = items.find('a')
+    if find_a:
+        Link = find_a['href'][4:]
+        PC.SpriritListSections.append(link + Link)
+
+req = requests.get(PC.SpriritListSections[0])
+soup = BeautifulSoup(req.content, 'html.parser')
+
+
+while True:
+    drink_box = soup.find_all('div', class_="product-container")
+    for items in drink_box:
+        find_a = items.find('a')
+        if find_a:
+            Link = find_a['href']
+            PC.SpiritDrinkList.append(link +Link)
+    next_button = soup.find('a', class_="btn-next")
+    if next_button:
+        x += 1
+        print(x)
+        req = requests.get(link + next_button['href'])
+    elif not next_button:
+        break
+
+for link in PC.SpiritDrinkList:
+    print(link)
+"""
 # Create a DataFrame with a link
 data = {'Yarn': ["Name"]}
 df = pd.DataFrame(data)
@@ -25,4 +64,4 @@ worksheet.write_url('B1', link, url_format, string='yes')
 
 # Save the Excel file
 writer._save()
-os.system(f'start excel {file_name}')
+os.system(f'start excel {file_name}')"""
