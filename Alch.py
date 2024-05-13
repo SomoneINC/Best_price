257195605ce13ba5a83211c4b3c39b3e3a9e239e
SomoneINC <72033209+SomoneINC@users.noreply.github.com>
2024-05-13 09:45:30 +0300

    Update Alch.py

--- Alch.py
+++ Alch.py
@@ -24,93 +24,89 @@ import pyperclip
 import platform
 
 
-def SpiritAndWine():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-infobars")
-    chrome_options.add_argument("--disable-features=VizDisplayCompositor")
-    chrome_options.add_argument("--disable-gpu")
-
-    SpritSelectionLinks = []
-    SpiritSelectionNames = []
-    ProductNames = []
-    ProductLinks = []
-    ProductPrices = []
-    service = Service(ChromeDriverManager().install())
-    link = "https://www.spiritsandwine.lv"
-    response = requests.get(link)
-    x = 0	
-    #Get all types of drink links
-    soup = BeautifulSoup(response.content, "html.parser")
-    a_tag = soup.find_all("a", class_="dropdown-item")
-    for a in a_tag:
-        if a["href"][:4] != "http" and x < 39: 
-            SpritSelectionLinks.append(link + a["href"])
-            print(link +a["href"])
-            x += 1
-
-    workbook = Workbook()
-
-    #Get all drink names, prices and valuemes of all the drinks
-    for section in SpritSelectionLinks :
-        soup = BeautifulSoup(requests.get(section).content, "html.parser")
-        productsCount = 1
-        title = soup.find("h1", class_="category-header-info").text
-        clean_title = re.sub(r"[/]", "", title)
-        sheet = workbook.create_sheet(title=clean_title)
-        while True:
-            slecetions_box = soup.find("div", class_="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xxl-4 g-0")
-            Links = slecetions_box.find_all("a")
-            for links in Links:
-                PLink = link + links["href"]
-                soup = BeautifulSoup(requests.get(PLink).content, "html.parser")
-                Name = soup.find("h1", class_="product-title").text
-                Sale = soup.find("div", class_="badge-sale")
+def Scraping(Link):
+    soup = BeautifulSoup(requests.get(Link).content, "html.parser")
+    productsCount = 1
+    title = soup.find("h1", class_="category-header-info").text
+    A.SpiritSelectionNames.append(title)
+    clean_title = re.sub(r"[/]", "", title)
+    sheet = A.workbook.create_sheet(title=clean_title)
+    while True:
+        slecetions_box = soup.find("div", class_="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xxl-4 g-0")
+        Links = slecetions_box.find_all("a")
+        for links in Links:
+            PLink = link + links["href"]
+            soup1 = BeautifulSoup(requests.get(PLink).content, "html.parser")
+            Name = soup1.find("h1", class_="product-title").text
+            Sale = soup1.find("div", class_="badge-sale")
+            
+            sheet.cell(row=productsCount, column=1).value = Name
+            if Sale is not None:
+                sheet.cell(row=productsCount, column=3).value = "Akcija"
+                Price = soup1.find("div", class_="actual")
                 
-                sheet.cell(row=productsCount, column=1).value = Name
-                if Sale is not None :
-                    sheet.cell(row=productsCount, column=3).value = "Akcija"
-                    Price = soup.find("div", class_="actual")
-                    
-                    if Price is not None :
-                        sheet.cell(row=productsCount, column=2).value = Price.text
-                    else :
-                        sheet.cell(row=productsCount, column=2).value = "Not found"
-                else :
-                    sheet.cell(row=productsCount, column=3).value = "Nav"
-                    Price = soup.find("div", class_="product-price mr-1 text-right").text
-                    sheet.cell(row=productsCount, column=2).value = Price
+                if Price is not None:
+                    sheet.cell(row=productsCount, column=2).value = Price.text
+                else:
+                    sheet.cell(row=productsCount, column=2).value = "Not found"
+            else:
+                sheet.cell(row=productsCount, column=3).value = "Nav"
+                Price = soup1.find("div", class_="product-price mr-1 text-right").text
+                sheet.cell(row=productsCount, column=2).value = Price
 
-                sheet.cell(row=productsCount, column=4).value = PLink
-                productsCount += 1 
-            NextPage = soup.find("a", class_="btn-next")
-            if  NextPage is not None and 'href' in NextPage.attrs :
-                soup = BeautifulSoup(requests.get(link + NextPage["href"]).content, "html.parser")
-                print("page next")
-            else :
-                break
-        
-        """
-        boxes = slecetions_box.find_all("h2")
-        for box in boxes:
-            print(box.text)"""
-        print(title + " done")
-        SpiritSelectionNames.append(title)
-    workbook.save("SpiritAndVine.xlsx")
+            sheet.cell(row=productsCount, column=4).value = PLink
+            productsCount += 1 
+            print(title + " " + str(productsCount))
+            
+        NextPage = soup.find("a", class_="btn-next")
+        if NextPage is not None and 'href' in NextPage.attrs:
+            soup = BeautifulSoup(requests.get(link + NextPage["href"]).content, "html.parser")
+            print("page next " + title)
+        else:
+            break
+class A:
+    workbook = Workbook()
+    SpiritSelectionNames = []
 
-    """
-    
-    
-    for section in SpritSelection :
-        clean_section = section.translate(str.maketrans("", "", string.punctuation))
-        sheet = workbook.create_sheet(title=clean_section)
-    """
-    print("Done")
-    
-   
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-infobars")
+chrome_options.add_argument("--disable-features=VizDisplayCompositor")
+chrome_options.add_argument("--disable-gpu")
 
-SpiritAndWine()
+SpritSelectionLinks = []
+
+ProductNames = []
+ProductLinks = []
+ProductPrices = []
+service = Service(ChromeDriverManager().install())
+link = "https://www.spiritsandwine.lv"
+response = requests.get(link)
+x = 0   
+# Get all types of drink links
+soup = BeautifulSoup(response.content, "html.parser")
+a_tag = soup.find_all("a", class_="dropdown-item")
+for a in a_tag:
+    if a["href"][:4] != "http" and x < 39: 
+        SpritSelectionLinks.append(link + a["href"])
+        print(link + a["href"])
+        x += 1
+
+threads = []
+# Get all drink names, prices and values of all the drinks
+for section in SpritSelectionLinks:
+    Functions = threading.Thread(target=Scraping, args=(section,))
+    Functions.start()
+    threads.append(Functions)
+
+for thread in threads:
+    thread.join()
+
+A.workbook.save("SpiritAndVine.xlsx")
+
+
+print("Done")
 
 
 """
